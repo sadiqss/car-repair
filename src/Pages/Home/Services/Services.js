35439833393d53b627d08366926d534ac2be304c
sadiqss <sadiq.sau48@gmail.com>
2022-04-15 14:48:50 +0100

    Responsive

--- src/Pages/Home/Services/Services.js
+++ src/Pages/Home/Services/Services.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import Service from '../Service';
+import './Services.css';
 
 
 const Services = () => {
@@ -11,14 +12,16 @@ const Services = () => {
     }, [])
     return (
         <div>
-            <h2>Services{services.length}</h2>
-            {
-                services.map(service => <Service
-                    key={service.id}
-                    service={service}
-                >
-                </Service>)
-            }
+            <h2 className='services-title'>Our Services</h2>
+            <div className="services-container">
+                {
+                    services.map(service => <Service
+                        key={service.id}
+                        service={service}
+                    >
+                    </Service>)
+                }
+            </div>
         </div>
     );
 };
